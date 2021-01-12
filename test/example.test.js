08aacd218bc5e10722936d13a91348ff817c4831
ERICH SAUER <erich.sauer@gmail.com>
2021-01-12 14:11:18 -0800

    adds test for compareNumber function

--- test/example.test.js
+++ test/example.test.js
@@ -1,16 +1,45 @@
 // IMPORT MODULES under test here:
+import { compareNumbers } from '../utils.js';
 // import { example } from '../example.js';
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+test('tests if 5 is the same as 5', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
+    const expected = 0;
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = false;
+    const actual = compareNumbers(5, 5);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+test('tests if 4 is less than as 5', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = -1;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = compareNumbers(5, 4);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+test('tests if 6 is greater than 5', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = +1;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = compareNumbers(5, 6);
 
     //Expect
     // Make assertions about what is expected versus the actual result
--- utils.js
+++ utils.js
@@ -1,11 +1,8 @@
-export function 
-// TDD a compareNumbers function that:
-
-// Has two parameters: guess and correctNumber
-// Returns one of the following values:
-// 0 - the numbers are identical
-// -1 - the guessed number is too low
-// 1 - the guessed number is too high
+export function compareNumbers(number1, number2) {
+    if (number1 === number2) return 0;
+    if (number1 > number2) return -1;
+    if (number1 < number2) return 1;
+}
 
 // Write one test at a time, each test should address one of the three possible outcomes.
 
