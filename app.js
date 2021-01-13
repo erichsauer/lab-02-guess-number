a051d3ea19928f2bdcc1435065942e39f091338c
ERICH SAUER <erich.sauer@gmail.com>
2021-01-12 19:12:32 -0800

    fancifies styling

--- app.js
+++ app.js
@@ -21,12 +21,13 @@ guessesDisplay.textContent = guessesRemaining;
 
 guessButton.addEventListener('click', () => {
     const userGuessNumber = userGuess.valueAsNumber;
-
+    guessFeedback.style.display = 'block';
+    
     if (userGuessNumber > 20) {
         alert('Woah there buckaroo. Gotta keep your guess below 20. Try again!');
         return;
     }
-
+    
     if (compareNumbers(randomNumber, userGuessNumber) === -1) {
         guessesRemaining--;
         numberOfGuesses++;
@@ -36,22 +37,23 @@ guessButton.addEventListener('click', () => {
     } else if (compareNumbers(randomNumber, userGuessNumber) === 1) {
         guessesRemaining--;
         numberOfGuesses++;
-
+        
         guessFeedback.textContent = 'Woops, too high! Guess again.';
         guessesDisplay.textContent = guessesRemaining;
     } else if (compareNumbers(randomNumber, userGuessNumber) === 0) {
         numberOfGuesses++;
         gameContainer.style.display = 'none';
         playAgainContainer.style.display = 'flex';
-
+        
         winLoseMessageDisplay.textContent = `Hooray! You won in ${numberOfGuesses} guesses!`;
         guessesDisplay.textContent = guessesRemaining;
     }
-
+    
     if (guessesRemaining === 0) {
         gameContainer.style.display = 'none';
         playAgainContainer.style.display = 'flex';
-
+        winLoseMessageDisplay.style.backgroundImage = "url('https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fcdn.wallpapersafari.com%2F76%2F75%2FW3DGBa.gif&f=1&nofb=1')";
+        
         winLoseMessageDisplay.textContent = `Oof! Let's try that again!`;
         guessesDisplay.textContent = guessesRemaining;
     }
@@ -61,7 +63,8 @@ playAgainButton.addEventListener('click', () => {
     
     gameContainer.style.display = 'flex';
     playAgainContainer.style.display = 'none';
-
+    guessFeedback.style.display = 'none';
+    
     guessesRemaining = 4;
     numberOfGuesses = 0;
     randomNumber = Math.ceil(Math.random() * 20);
